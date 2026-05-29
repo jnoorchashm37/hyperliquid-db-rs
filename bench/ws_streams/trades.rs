4ef359bf2a8271f7b7fc0ff991d0b30c4ab222ee
jnoorchashm37 <joseph@sorellalabs.xyz>
2026-05-28 23:45:10 -0400

    fixed timestamp thing

--- bench/ws_streams/trades.rs
+++ bench/ws_streams/trades.rs
@@ -19,7 +19,7 @@ use crate::ws_streams::utils::{
     set_hl_websocket_read_timeout, spawn_hl_trades_websocket, spawn_hl_watcher, timestamp_utc
 };
 
-const TIMEOUT_SECS: u64 = 60;
+const TIMEOUT_SECS: u64 = 300;
 const PUBLIC_WS_READ_TIMEOUT_MS: u64 = 100;
 const TRADES_COIN: &str = "BTC";
 static IS_RUNNING: AtomicBool = AtomicBool::new(true);
