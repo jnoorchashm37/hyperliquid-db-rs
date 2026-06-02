f74d2e7fa9375a2273e5394bec093763f7913fe6
jnoorchashm37 <joseph@sorellalabs.xyz>
2026-06-02 13:15:47 -0400

    removed old files

--- hyperliquid-db-core/src/fs_handlers/mod.rs
+++ hyperliquid-db-core/src/fs_handlers/mod.rs
@@ -3,7 +3,6 @@ pub mod directory_watcher;
 #[cfg(target_os = "linux")]
 pub use directory_watcher::DirectoryWatcher;
 
-mod file;
 #[cfg(not(target_os = "linux"))]
 mod stub;
 #[cfg(not(target_os = "linux"))]
