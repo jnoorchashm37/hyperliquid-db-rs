b1367f7d1c9386b53d1e725072393c11f24341e0
jnoorchashm37 <joseph@sorellalabs.xyz>
2026-06-02 15:26:57 -0400

    l2 book

--- hyperliquid-db-core/src/types/l2_orderbook.rs
+++ hyperliquid-db-core/src/types/l2_orderbook.rs
@@ -1,6 +1,25 @@
-use std::collections::BTreeMap;
-
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
-pub struct L2Book {}
+#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Serialize, Deserialize, Hash)]
+pub struct L2Book {
+    pub coin:   String,
+    pub levels: [Vec<L2BookLevel>; 2],
+    pub time:   u64
+}
+
+impl L2Book {
+    pub fn bids(&self) -> &[L2BookLevel] {
+        &self.levels[0]
+    }
+
+    pub fn asks(&self) -> &[L2BookLevel] {
+        &self.levels[1]
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Serialize, Deserialize, Hash)]
+pub struct L2BookLevel {
+    pub px: String,
+    pub sz: String,
+    pub n:  u64
+}
