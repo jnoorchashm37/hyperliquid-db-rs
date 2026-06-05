cf5f4bc278be08b7106ef259a1f8d84707a6c679
jnoorchashm37 <joseph@sorellalabs.xyz>
2026-06-05 16:20:04 -0400

    proper directory

--- hyperliquid-db-core/src/lib.rs
+++ hyperliquid-db-core/src/lib.rs
@@ -9,4 +9,4 @@ pub mod processors;
 pub mod fs_handlers;
 pub mod hl_fs;
 
-pub const HYPERLIQUID_DATA_DIR: &str = "/var/lib/hyperliquid/hl/data";
+pub const HYPERLIQUID_DATA_DIR: &str = "/root/hl/data";
